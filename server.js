30e849fc3b2b2e82c72ae8d936501baa0a7ef4e9
Suryakant kakkar <19BCS4651@cuchd.in>
2025-03-09 23:08:33 +0530

    fix

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Define your routes
 app.use("/api/destinations", require("./routes/destinationRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+// Define a default route
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
